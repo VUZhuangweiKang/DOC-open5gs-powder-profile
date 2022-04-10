3cdb579a161cc8ae34e4e856f32aced24e2c56d1
ZhuangweiKang <zkty9@mail.missouri.edu>
2022-04-10 14:37:53 -0500

    update

--- src/manager.sh
+++ src/manager.sh
@@ -3,18 +3,17 @@
 open5gs_node="pc821"
 ran_node="pc494"
 # ue_nodes=("pc488" "pc516" "pc500" "pc519")
-ue_nodes=("pc488")
+ue_nodes=("pc516")
 
 NUM_UE=$1
-repo="~/repository"
+repo="/local/repository"
 
 # step 1. start UEs on each node
 num_ue_nodes=${#ue_nodes[@]}
 lower=0
 for node in "${ue_nodes[@]}"; do
     step=`expr $NUM_UE / $num_ue_nodes`
-    upper=$[$lower+$step]
-    upper=$((lower+$step))
+    upper=$((lower+step))
     echo $lower $upper
     ssh -p 22 VUZK@$node.emulab.net bash -c $repo/src/ue.sh $lower $upper
     lower=$((lower+step))
